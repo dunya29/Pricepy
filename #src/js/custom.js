dced3ab5e309d97f506230d9c7fd0d60969de4ff
Dunya <dunya2928@mail.ru>
2024-10-16 18:17:17 +0300

    menu

--- #src/js/custom.js
+++ #src/js/custom.js
@@ -1,34 +1,108 @@
+const allModals = document.querySelectorAll(".js-modal")
 const checkPhoneMod = document.querySelector(".check-phoneMod")
 const checkPhoneModInp = document.querySelector(".check-phoneMod__code input")
 const checkPhoneModBtn = document.querySelector(".check-phoneMod .request__btn")
-let valTimeout
+const successModal = document.querySelector(".success-mod")
+const errorModal = document.querySelector(".error-mod")
+//open modal
+function openModal(modal) {
+    $(modal).fadeIn(500);
+}
+//close modal
+function closeModal(modal) {
+    $('.js-modal').fadeOut(100);
+}
+// modal click outside
+if (allModals.length > 0) {
+    allModals.forEach(item => {
+        if (item.querySelector(".modal__content")) {
+            item.addEventListener("click", e => {
+                if (!item.querySelector(".modal__content").contains(e.target)) {
+                    closeModal(item)
+                }
+            })
+        }
+    })
+}
+//setSuccessTxt
+function setSuccessTxt(title = false, txt = false) {
+    successModal.querySelector(".modal__title span").textContent = title ? title : "Заявка отправлена" 
+    if (txt) {
+      successModal.querySelector(".modal__title p").textContent = txt
+    }
+}
+//setErrorTxt
+function setErrorTxt(title = false, txt = false) {
+    errorModal.querySelector(".modal__title span").textContent = title ? title : "Что-то пошло не так" 
+    if (txt) {
+        errorModal.querySelector(".modal__title p").textContent = txt
+    }
+}
+// openSuccessMod
+function openSuccessMod(title = false, txt = false) {
+    setSuccessTxt(title,txt)
+    openModal(successModal)
+}
+// openErrorMod
+function openErrorMod(title = false, txt = false) {
+    setErrorTxt(title,txt)
+    openModal(errorModal)
+}
+// formSuccess
+function formSuccess(form, title = false, txt = false) {
+    form.querySelectorAll(".item-form").forEach(item => item.classList.remove("error"))
+    form.querySelectorAll("input").forEach(inp => {
+        inp.classList.remove("has-error")
+        if (!["hidden", "checkbox", "radio"].includes(inp.type)) {
+            inp.value = ""
+        }
+    })
+    openSuccessMod(title, txt)
+}
+let codeResTimeout
 function openCheckPhoneMod() {
-    if (checkPhoneMod) {   
-        $(checkPhoneMod).fadeIn(500);
-        let val = 30
-        document.querySelector(".check-phoneMod__codeResent").innerHTML = `Повторный запрос кода доступен через <span>${val}</span> сек.`
+    clearTimeout(codeResTimeout)
+    openModal(checkPhoneMod)
+    let val = 30  
+    document.querySelector(".check-phoneMod__codeResent").innerHTML = `Повторный запрос кода доступен через <span>${val}</span> сек.`
+    setTimeout(() => {
         function changeTimeVal() {
-            clearTimeout(valTimeout)
             document.querySelector(".check-phoneMod__codeResent span").textContent = val
             val--
             if ( val > 0) {
-                valTimeout = setTimeout(changeTimeVal, 1000);
+                codeResTimeout = setTimeout(changeTimeVal, 1000);
             } else {
                 document.querySelector(".check-phoneMod__codeResent").innerHTML = `<button type="button" class="check-phoneMod__resent">Отправить  новый код</button>`
             }
         }
         changeTimeVal()
-    }
+    }, 500);
 }
-if (checkPhoneMod) {
-    checkPhoneMod.addEventListener("click", e => {
-        if (!checkPhoneMod.querySelector(".modal__content").contains(e.target)) {
-            checkPhoneMod.querySelector(".js-modal-close").click()
-        }
-    })
+if (checkPhoneModInp) {
     function checkVal() {
         checkPhoneModInp.value.length == 0 ? checkPhoneModBtn.setAttribute("disabled", true) : checkPhoneModBtn.removeAttribute("disabled")
     }
     checkVal()
     checkPhoneModInp.addEventListener("input",checkVal)
+}
+const headermobBtn = document.querySelector(".header__mobBtn")
+if (headermobBtn) {
+    headermobBtn.addEventListener('click', () => document.querySelector(".header__phones").classList.add("open"))
+    document.querySelector(".header__phones-close").addEventListener('click', () => {
+        document.querySelector(".header__phones").classList.remove("open")
+    })
+}
+//fixed-menu
+const fixedMenu = document.querySelector(".fixed-menu")
+function showFixedMenu() {
+  if (window.innerWidth <= 767.98 && window.innerHeight - document.querySelector(".footer").getBoundingClientRect().bottom + 150 <= 0) {
+    fixedMenu.classList.remove("unshow")
+  } else {
+    fixedMenu.classList.add("unshow")
+  }
+}
+if (fixedMenu) {
+  showFixedMenu()
+  window.addEventListener("resize", showFixedMenu)
+  window.addEventListener("scroll", showFixedMenu)
 }
--- pricepy/js/scripts.js
+++ pricepy/js/scripts.js
@@ -55,7 +55,7 @@ $(function() {
     $('.burger').click(function(e) {
         e.preventDefault();
         $(this).toggleClass('open');
-        $('.menu').slideToggle();
+        $('.navbar__menu').slideToggle();
     });
 
 
