fcd8a416390ee0eaecc842d106ccabc6f18586e9
Dunya <dunya2928@mail.ru>
2024-10-16 14:14:10 +0300

    title

--- pricepy/js/custom.js
+++ pricepy/js/custom.js
@@ -1,15 +1,18 @@
 const checkPhoneMod = document.querySelector(".check-phoneMod")
 const checkPhoneModInp = document.querySelector(".check-phoneMod__code input")
 const checkPhoneModBtn = document.querySelector(".check-phoneMod .request__btn")
+let valTimeout
 function openCheckPhoneMod() {
-    if (checkPhoneMod) {
+    if (checkPhoneMod) {   
         $(checkPhoneMod).fadeIn(500);
         let val = 30
+        document.querySelector(".check-phoneMod__codeResent").innerHTML = `Повторный запрос кода доступен через <span>${val}</span> сек.`
         function changeTimeVal() {
+            clearTimeout(valTimeout)
             document.querySelector(".check-phoneMod__codeResent span").textContent = val
             val--
             if ( val > 0) {
-                setTimeout(changeTimeVal, 1000);
+                valTimeout = setTimeout(changeTimeVal, 1000);
             } else {
                 document.querySelector(".check-phoneMod__codeResent").innerHTML = `<button type="button" class="check-phoneMod__resent">Отправить  новый код</button>`
             }
