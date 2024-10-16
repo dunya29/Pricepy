72d3aad15390713874d1be39be501e4a113aaeea
Dunya <dunya2928@mail.ru>
2024-10-16 14:00:59 +0300

    last

--- #src/js/custom.js
+++ #src/js/custom.js
@@ -1,6 +1,22 @@
 const checkPhoneMod = document.querySelector(".check-phoneMod")
 const checkPhoneModInp = document.querySelector(".check-phoneMod__code input")
 const checkPhoneModBtn = document.querySelector(".check-phoneMod .request__btn")
+function openCheckPhoneMod() {
+    if (checkPhoneMod) {
+        $(checkPhoneMod).fadeIn(500);
+        let val = 30
+        function changeTimeVal() {
+            document.querySelector(".check-phoneMod__codeResent span").textContent = val
+            val--
+            if ( val > 0) {
+                setTimeout(changeTimeVal, 1000);
+            } else {
+                document.querySelector(".check-phoneMod__codeResent").innerHTML = `<button type="button" class="check-phoneMod__resent">Отправить  новый код</button>`
+            }
+        }
+        changeTimeVal()
+    }
+}
 if (checkPhoneMod) {
     checkPhoneMod.addEventListener("click", e => {
         if (!checkPhoneMod.querySelector(".modal__content").contains(e.target)) {
